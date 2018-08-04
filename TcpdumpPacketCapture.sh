8aa9952e0d2d54b9fe644f5cfb5dd0ad38a57a9f
wedgetaileagle <gregshend@gmail.com>
2018-08-04 11:31:21 +1000

    Update TcpdumpPacketCapture.sh

--- TcpdumpPacketCapture.sh
+++ TcpdumpPacketCapture.sh
@@ -1,19 +1,11 @@
+#!/bin/bash
 LOOPMAX='2'
-
 DURATION='15'
-
 TCPORT='20104'
-
 SERVERNAME="$(uname -a | cut -b '7-17')"
-
- 
 
 cd /tmp
 
- 
-
 for I in $(seq 1 $LOOPMAX); do
-
     timeout $DURATIONm tcpdump -w "$SERVERNAME_TCPort$TCPORT_$(date +%m%d%H%M)" 'tcp port $TCPORT'
-
 done
